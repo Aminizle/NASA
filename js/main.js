53d966ca541d1b8c50325ea0b187d98d864b37c9
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-04-10 04:32:32 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', getImage)
+window.onload = getImage;
+
+
+const key = "JU7IDnJ2gDoCgPj1aE6mcfSmqtgIkDwcyKXp9eTD"
+
+
+function getImage() {
+    
+var date = document.querySelector('#input').value
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`)
+    .then( res => res.json())
+    .then( data => {
+        console.log(data)
+        console.log(date)
+        document.querySelector('.name').innerText = data.title        
+        document.querySelector('.description').innerText = data.explanation
+        document.querySelector('img').src = data.hdurl
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+        
+    });
+}
